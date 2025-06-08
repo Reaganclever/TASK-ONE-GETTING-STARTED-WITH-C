e70fe7bf6306ac7a064e016a4fee323b8cf95512
reaganclever <kasulereagan86@gmail.com>
2025-06-08 09:49:42 +0300

    Intital

---
+++ variableDeclaration.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "TaskOneProject.h"
+
+void declareAndPrintVariables(){
+
+int age = 20;
+float height = 5.9f;
+double distance = 12345.6789;
+char initial = 'A';
+
+printf("AGE:  %d\n" , age);
+printf("HEIGHT:  %f\n" , height);
+printf("DISTANCE:  %lf\n" , distance);
+printf("INITIAL:   %c\n" , initial);
+
+}
+
