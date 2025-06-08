3f41842d53345c27f68eec3c5821736e0c29c6da
reaganclever <kasulereagan86@gmail.com>
2025-06-08 10:04:37 +0300

    TOGSWC1-4 COMMIT#1 ON BRANCH#3

--- variableDeclaration.c
+++ variableDeclaration.c
@@ -5,12 +5,12 @@ void declareAndPrintVariables(){
 
 int age = 29;
 float height = 5.4570f;
-double distance = 12345.6789;
+double distance = 12345.39;
 char initial = 'A';
 
 printf("NEWEST AGE:  %d\n" , age);
 printf(" NEW ORIGINAL HEIGHT:  %f\n" , height);
-printf("DISTANCE:  %lf\n" , distance);
+printf("NEW DISTANCE:  %lf\n" , distance);
 printf("INITIAL:   %c\n" , initial);
 
 }
